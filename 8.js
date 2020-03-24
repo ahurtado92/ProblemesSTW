f96d9746299e19d2f7a71ebe808d1df812d99b37
Paula Sarqui <pfsarqui@gmail.com>
2020-03-24 19:06:54 +0100

    Implementa el ejercicio 8 Las instrucciones asíncronas se envuelven con una promesa.

--- 8.js
+++ 8.js
@@ -1,7 +1,27 @@
-let llista = [];
+const fs = require('fs');
 
-f6 = function (llista,callback) {
-    //llista.forEach(...);
+f6 = function (llista, callback) {
+    let result = [];
+
+    let readFiles = new Promise((resolve, reject) => {
+        llista.forEach(function (element, index, array) {
+            fs.readFile('./' + element, 'utf8', function (err, data) {
+                if (err) {
+                    throw err;
+                }
+                console.log('data: ' + data);
+                result.push(data);
+
+                if (index === array.length - 1) {
+                    resolve();
+                }
+            });
+        });
+    });
+
+    readFiles.then(() => {
+        callback(result);
+    });
 };
 
 f6(['a1.txt','a2.txt'],function (result) {
