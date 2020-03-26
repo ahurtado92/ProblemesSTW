9dbbeaaf4280141c11ae958857820853be4c548d
tonioel <antonio.hurtadoo@e-campus.uab.cat>
2020-03-26 21:37:30 +0100

    Commit

--- 19.js
+++ 19.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 when = function (f1){
+    if (!(this instanceof when)) return new when(f1);
     this.do = function (f2) {
         f1(f2);
     }
@@ -9,4 +10,4 @@ when = function (f1){
 var f1 = function (callback) { fs.readFile('a1.txt', callback) }
 var f2 = function (error, result) { console.log(result.toString()) }
 
-new when(f1).do(f2);
+when(f1).do(f2);
