abc921a1f98852fb2b4df04414b96897a723f22e
tonioel <antonio.hurtadoo@e-campus.uab.cat>
2020-03-26 21:31:49 +0100

    DOES NOT WORK!

---
+++ 19.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+when = function (f1){
+    this.do = function (f2) {
+        f1(f2);
+    }
+}
+
+var f1 = function (callback) { fs.readFile('a1.txt', callback) }
+var f2 = function (error, result) { console.log(result) }
+
+new when(f1).do(f2);
