ac3b7ac754bc94f80015821582e3662d20067a71
Paula Sarqui <pfsarqui@gmail.com>
2020-03-27 20:54:17 +0100

    Modifica el ejercicio 25
    Estaba identado por tabulaciones y pasa a estar identado con 4 espacios.

--- 25.js
+++ 25.js
@@ -5,20 +5,20 @@ f1 = x => Promise.resolve(1);
 
 //p = Promise.resolve(0);
 
-p.then(f1).catch(f2)
-p.then(f1,f2).catch()
+p.then(f1).catch(f2);
+p.then(f1,f2).catch();
 
 var promiseToCallback = function(f) {
 	return function(x, callback) {
 		f(x).then(res => {
-				callback(null, r)
+				callback(null, r);
 			},
-			err => {callback(err, null)}
+			err => {callback(err, null);}
 		)			
 	}
-}
+};
 
-f(x).then(XXX)
-g(x, function(err, res) {res = XXX })
+f(x).then(XXX);
+g(x, function(err, res) {res = XXX });
 
-g = promiseToCallback(f)
+g = promiseToCallback(f);
