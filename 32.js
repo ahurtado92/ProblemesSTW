3348a355b78b5e1e829b7b82d02c099b45393df9
tonioel <antonio.hurtadoo@e-campus.uab.cat>
2020-03-29 19:54:15 +0200

    Commit

--- 32.js
+++ 32.js
@@ -27,4 +27,14 @@ let promiseBarrier = function(n) {
 	return list;
 };
 
-let list = promiseBarrier(3);
+var [f1, f2] = promiseBarrier(2);
+Promise.resolve(0)
+	.then(x => { console.log("c1 s1"); return x; })
+	.then(x => { console.log("c1 s2"); return x; })
+	.then(x => { console.log("c1 s3"); return x; })
+	.then(x => { console.log("c1 s4"); return x; })
+	.then(f1);
+Promise.resolve(0)
+	.then(f2)
+	.then(x => { console.log("c2 s1"); return x; })
+	.then(x => { console.log("c2 s2"); return x; });
