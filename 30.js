da2de1333f66633a68be3e4183f64a38970be100
Paula Sarqui <pfsarqui@gmail.com>
2020-03-29 19:04:12 +0200

    Modifica el ejercicio 30
    Lo he modificado para que funcionara, ahoraa la función que debíamos implementar retorna otra función que, a si vez, retorna una promesa que gestiona la ejecución de las funciones que retornan una promesa.

--- 30.js
+++ 30.js
@@ -1,10 +1,21 @@
+let functionPromiseComposer = (function (f1, f2) {
+    return (function (x) {
+        return new Promise((resolve, reject) => {
+            f2(x).then((result) => {
+                f1(result).then((result) => {
+                    resolve(result);
+                }).catch((result) => {
+                    reject(result);
+                });
+            }).catch((result) => {
+                reject(result);
+            });
+        });
+    });
+});
 
+let f1 = x => new Promise((resolve, reject) => resolve(x+1));
+functionPromiseComposer(f1, f1)(3).then(console.log);
 
-var functionPromiseComposer = function(f1, f2) {
-    var f3 = function (x) {
-        return f2(x).then(f1);
-    }
-}
-
-var f1 = x => new Promise((resolve, reject) => resolve(x+1));
-functionPromiseComposer(f1, f1);
+let f3 = x => new Promise((resolve, reject) => reject('always fails'));
+functionPromiseComposer(f1, f3)(3).catch(console.log);
