7d11ebdda2a4c7aa25ec79d36409f2b8b84c6f95
tonioel <antonio.hurtadoo@e-campus.uab.cat>
2020-03-29 20:27:58 +0200

    Exercici 32

--- 32.js
+++ 32.js
@@ -39,3 +39,24 @@ Promise.resolve(0)
 	.then(f2)
 	.then(x => { console.log("c2 s1"); return x; })
 	.then(x => { console.log("c2 s2"); return x; });
+
+var [f1, f2, f3] = promiseBarrier(3);
+Promise.resolve(0)
+	.then(x => { console.log("c1 s1"); return x; })
+	.then(x => { console.log("c1 s2"); return x; })
+	.then(x => { console.log("c1 s3"); return x; })
+	.then(f1)
+	.then(x => { console.log("c1 s4"); return x; })
+Promise.resolve(0)
+	.then(x => { console.log("c2 s1"); return x; })
+	.then(f2)
+	.then(x => { console.log("c2 s2"); return x; })
+Promise.resolve(0)
+	.then(f3)
+	.then(x => { console.log("c3 s1"); return x; })
+	.then(x => { console.log("c3 s2"); return x; })
+	.then(x => { console.log("c3 s3"); return x; })
+
+var [f1, f2] = promiseBarrier(2);
+Promise.resolve(1).then(f1).then(console.log)
+Promise.resolve(2).then(f2).then(console.log)
