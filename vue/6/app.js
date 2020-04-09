d860e41a899897985f5e0353b3edda30e51ee446
tonioel <antonio.hurtadoo@e-campus.uab.cat>
2020-04-09 16:24:14 +0200

    Ejercicio 6 funciona, pero no cambia el color de AM I RED?

--- vue/6/app.js
+++ vue/6/app.js
@@ -1,13 +1,15 @@
 var vm = new Vue({
     el: '#app',
     data: {
-      redness: "",
+      redness: '',
+      activeColor: 'hsl(0,' + this.redness+ '%,50%)' ,
     },
     template: `
         <div>
-            <div v-bind:style="color: hsl(0,'{{redness}}'%,50%)">AM I RED?</div>
-            <input type="range" min="1" max="100">
-            <div v-if="">YES!</div>
+            <!--<div v-bind:style="color: hsl(0,'{{redness}}'%,50%)">AM I RED?</div>-->
+            <div v-bind:style="{ color: activeColor }" >AM I RED?</div>
+            <input type="range" min="1" max="100" v-model="redness">
+            <div v-if="redness >= 70">YES!</div>
         </div>
     `,
 });
