bc18e6d5a5e3408e7664b24d5dcdee3094390f35
tonioel <antonio.hurtadoo@e-campus.uab.cat>
2020-04-16 14:48:20 +0200

    Estructura ejercicio 15 - Falta implementar

---
+++ vue/15/app.js
@@ -0,0 +1,21 @@
+Vue.component('magic-input', {
+
+    template: `
+    `,
+
+});
+
+var vm = new Vue({
+    el: '#app',
+    data: {
+            text: "Hola"
+    },
+    template: `
+        <div>
+        <magic-input v-model="text" id="name"></magic-input>
+        <input v-model="text">
+        {{text}}
+        </div>`,
+});
+
+
