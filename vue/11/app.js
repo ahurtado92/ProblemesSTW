f40cc4e820700184ddc1b46bb75c4c113fd49099
tonioel <antonio.hurtadoo@e-campus.uab.cat>
2020-04-16 03:39:28 +0200

    Ejercicio 11

--- vue/11/app.js
+++ vue/11/app.js
@@ -5,24 +5,21 @@ Vue.component('words-to-list', {
             strings: [  ],
         }
     },
-
+    mounted: function() {
+        this.transform();
+    },
     methods: {
-        print: function () {
+        transform: function () {
             var wtl = document.getElementById('words-to-list');
             var words = wtl.getAttribute('words');
             this.strings = words.split(" ");
-            console.log(this.strings);
         }
     },
-    //TODO: Ejercicio 11
-    // Funciona, pero a través de un botón que dispara una función.
-    // Se ha de implementar para que realice la función de forma automática.
 
     template: `
         <div>
             <ul>
                 <li v-for="string in strings" >{{string}}</li>
-                <button v-on:click="print">Convert</button>
             </ul>
             
         </div>`,
