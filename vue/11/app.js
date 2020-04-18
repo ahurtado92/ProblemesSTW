09b1cce97defc674a61be4724b08b06b6f75f1e0
tonioel <antonio.hurtadoo@e-campus.uab.cat>
2020-04-18 17:50:57 +0200

    Problema 11

--- vue/11/app.js
+++ vue/11/app.js
@@ -2,24 +2,18 @@ Vue.component('words-to-list', {
 
     data: function () {
         return {
-            strings: [  ],
+            wordsListSplit: this.words.split(' ')
         }
     },
-    mounted: function() {
-        this.transform();
-    },
-    methods: {
-        transform: function () {
-            var wtl = document.getElementById('words-to-list');
-            var words = wtl.getAttribute('words');
-            this.strings = words.split(" ");
-        }
+
+    props: {
+        words: String,
     },
 
     template: `
         <div>
             <ul>
-                <li v-for="string in strings" >{{string}}</li>
+                <li v-for="string in strings" >{{wordsListSplit}}</li>
             </ul>
             
         </div>`,
